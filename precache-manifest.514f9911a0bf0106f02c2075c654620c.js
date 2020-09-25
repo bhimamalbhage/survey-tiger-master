73526cbaf49a9e12c534c8626336643957de90b4
Bhimashankar <bhimamalbhage@hotmail.com>
2020-09-25 12:33:18 +0530

    Updates

---
+++ precache-manifest.514f9911a0bf0106f02c2075c654620c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ccbd15bb81875af940fb5cd5eeed333",
+    "url": "/survey-tiger-master/index.html"
+  },
+  {
+    "revision": "23ad938d4716bf6d4744",
+    "url": "/survey-tiger-master/static/css/main.ab43e74c.chunk.css"
+  },
+  {
+    "revision": "0ddbdf7d62647fe28b79",
+    "url": "/survey-tiger-master/static/js/2.4f66e2ca.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/survey-tiger-master/static/js/2.4f66e2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ad938d4716bf6d4744",
+    "url": "/survey-tiger-master/static/js/main.53b03ae1.chunk.js"
+  },
+  {
+    "revision": "97468ee596494c23560d",
+    "url": "/survey-tiger-master/static/js/runtime-main.f1e834d5.js"
+  }
+]);
